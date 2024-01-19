ec0e856eb0eedb4268cd456ba1ac53999b2c0ae8
Huijie Wei <huijiewei@outlook.com>
2024-01-19 22:52:10 +0800

    feat(ui): add Icon component

---
+++ packages/ui/src/components/icon/Icon.tsx
@@ -0,0 +1,53 @@
+import { __DEV__, isString } from '@resolid-remix/utils';
+import type { SVGAttributes } from 'react';
+import type { Size } from '../../utils/types';
+
+export type IconProps = SVGAttributes<SVGElement> & { size?: number | string | Size };
+
+const iconSizes = {
+  xs: 16,
+  sm: 20,
+  md: 24,
+  lg: 28,
+  xl: 32,
+};
+
+export const Icon = (props: IconProps) => {
+  const {
+    fill = 'none',
+    viewBox = '1 1 22 22',
+    stroke = 'currentColor',
+    strokeWidth = 2,
+    strokeLinecap = 'round',
+    strokeLinejoin = 'round',
+    children,
+    size,
+    width,
+    height,
+    ...rest
+  } = props;
+
+  const sizeValue =
+    isString(size) && ['xs', 'sm', 'md', 'lg', 'xl'].includes(size) ? iconSizes[size as Size] : size ?? '1em';
+
+  return (
+    <svg
+      fill={fill}
+      width={width ?? sizeValue}
+      height={height ?? sizeValue}
+      viewBox={viewBox}
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      strokeLinecap={strokeLinecap}
+      strokeLinejoin={strokeLinejoin}
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      {children}
+    </svg>
+  );
+};
+
+if (__DEV__) {
+  Icon.displayName = 'Icon';
+}
