bc3e081717e3f39ea52c2e1333e0a867f8814aa9
Huijie Wei <huijiewei@outlook.com>
2024-01-19 16:46:49 +0800

    fix(website): remove manualChunks option to fix build

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,49 +1,20 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig, splitVendorChunkPlugin, type UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 import { stylexPlugin } from 'vite-plugin-stylex-dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // noinspection JSUnusedGlobalSymbols
-export default defineConfig(({ isSsrBuild }) => {
-  const config: UserConfig = {
-    plugins: [
-      remix({
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-        },
-      }),
-      Inspect(),
-      tsconfigPaths(),
-      stylexPlugin(),
-      !isSsrBuild && splitVendorChunkPlugin(),
-    ],
-  };
-
-  if (!isSsrBuild) {
-    config.build = {
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            if (
-              id.includes('/node_modules/react/') ||
-              id.includes('/node_modules/react-dom/') ||
-              id.includes('/node_modules/react-is/') ||
-              id.includes('/node_modules/scheduler/') ||
-              id.includes('/node_modules/prop-types/')
-            ) {
-              return 'react';
-            }
-
-            if (id.includes('/node_modules/')) {
-              return 'vendor';
-            }
-          },
-        },
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
       },
-    };
-  }
-
-  return config;
+    }),
+    Inspect(),
+    tsconfigPaths(),
+    stylexPlugin(),
+  ],
 });
