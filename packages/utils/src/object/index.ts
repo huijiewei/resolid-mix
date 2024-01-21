3ec1a08cadbd5ef0488549871ae0afc6db6061f8
Huijie Wei <huijiewei@outlook.com>
2024-01-21 10:23:52 +0800

    feat(utils): add hasOwnProperty object util

--- packages/utils/src/object/index.ts
+++ packages/utils/src/object/index.ts
@@ -7,3 +7,11 @@ export const omit = <T extends object, K extends keyof T>(object: T, keys: K[]) 
     { ...object },
   ) as Omit<T, K>;
 };
+
+export const hasOwnProperty = <T extends object>(object: T, prop: string | number | symbol): prop is keyof T => {
+  if (typeof Object.hasOwn === 'function') {
+    return Object.hasOwn(object, prop);
+  }
+
+  return Object.prototype.hasOwnProperty.call(object, prop);
+};
