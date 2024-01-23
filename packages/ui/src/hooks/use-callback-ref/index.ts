05c1ed0dfec27517e703f56cd4c3bc4c1c3fede8
Huijie Wei <huijiewei@outlook.com>
2024-01-23 16:39:19 +0800

    feat(ui): add some hooks

---
+++ packages/ui/src/hooks/use-callback-ref/index.ts
@@ -0,0 +1,17 @@
+import { useCallback, useRef, type DependencyList } from 'react';
+import { useIsomorphicEffect } from '../use-isomorphic-effect';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const useCallbackRef = <T extends (...args: any[]) => any>(
+  callback: T | undefined,
+  deps: DependencyList = [],
+): T => {
+  const ref = useRef(callback);
+
+  useIsomorphicEffect(() => {
+    ref.current = callback;
+  });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useCallback(((...args) => ref.current?.(...args)) as T, deps);
+};
