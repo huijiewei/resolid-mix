ca7d6edd7597636d4d4b14b10a5cc867b6d85463
Huijie Wei <huijiewei@outlook.com>
2024-01-26 16:30:43 +0800

    feat(ui): add CloseButton component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './button/Button';
 export * from './button/ButtonGroup';
+export * from './close-button/CloseButton';
 export * from './icon/Icon';
 export * from './slot/Slot';
 export * from './visually-hidden/VisuallyHidden';
