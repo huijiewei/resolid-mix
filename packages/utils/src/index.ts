2ad5cf2c31d99de5fbfe4e37aad8a119806303d7
Huijie Wei <huijiewei@outlook.com>
2024-01-27 13:13:58 +0800

    feat(utils): add number module with isNumber and clamp functions

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,7 @@
 export * from './dom';
 export * from './env';
 export * from './function';
+export * from './number';
 export * from './object';
 export * from './promise';
 export * from './string';
