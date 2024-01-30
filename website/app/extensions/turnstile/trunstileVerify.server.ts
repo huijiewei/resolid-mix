d08f1d60a577e0f07b8e7bdc5d92c0730ea9acc0
Huijie Wei <huijiewei@outlook.com>
2024-01-30 16:02:33 +0800

    feat(website): add Cloudflare Trunstile component

---
+++ website/app/extensions/turnstile/trunstileVerify.server.ts
@@ -0,0 +1,26 @@
+type TokenValidateResponse = {
+  'error-codes': string[];
+  success: boolean;
+  action: string;
+  cdata: string;
+};
+
+export const trunstileVerifyServer = async (token: string) => {
+  const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      response: token,
+      secret: process.env.NXT_TURNSTILE_SECRET,
+    }),
+  });
+
+  const data: TokenValidateResponse = await response.json();
+
+  return {
+    success: data.success,
+    error: data['error-codes']?.length ? data['error-codes'][0] : null,
+  };
+};
