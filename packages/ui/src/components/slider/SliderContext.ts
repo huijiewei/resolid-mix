624cd676fcce4a1cef6244f9991d6c50f06a3801
Huijie Wei <huijiewei@outlook.com>
2024-01-30 21:44:27 +0800

    feat(ui): add Slider component

---
+++ packages/ui/src/components/slider/SliderContext.ts
@@ -0,0 +1,96 @@
+import type { ReactNode } from 'react';
+import { createContext } from '../../utils/context';
+import type { Color, Size } from '../../utils/types';
+
+export type SliderValue = number | [number, number];
+
+export type SliderBaseProps = {
+  /**
+   * The minimum allowed value of the slider. Cannot be greater than max.
+   * @default 0
+   */
+  min?: number;
+
+  /**
+   * The maximum allowed value of the slider. Cannot be less than min.
+   * @default 100
+   */
+  max?: number;
+
+  /**
+   * The step in which increments/decrements have to be made
+   * @default 1
+   */
+  step?: number;
+
+  /**
+   * Disabled
+   * @default false
+   */
+  disabled?: boolean;
+
+  /**
+   * Reverse
+   * @default false
+   */
+  reverse?: boolean;
+
+  /**
+   * Vertical
+   * @default false
+   */
+  vertical?: boolean;
+
+  /**
+   * Ticks
+   * @default false
+   */
+  ticks?: boolean | number[];
+
+  /**
+   * Masks
+   */
+  marks?: { value: number; label?: ReactNode }[];
+
+  /**
+   * The size of the Slider
+   * @default 'md'
+   */
+  size?: Size;
+
+  /**
+   * Color
+   * @default 'primary'
+   */
+  color?: Color;
+};
+
+type SliderContext = Required<Omit<SliderBaseProps, 'marks'>> & Pick<SliderBaseProps, 'marks'>;
+
+const [SliderProvider, useSlider] = createContext<SliderContext>({
+  strict: true,
+  name: 'SliderContext',
+});
+
+export { SliderProvider, SliderThumbProvider, SliderValueProvider, useSlider, useSliderThumb, useSliderValue };
+
+type SliderValueContext = {
+  value: SliderValue;
+  onChange: (value: SliderValue) => void;
+  onChangeEnd: (value: SliderValue) => void;
+};
+
+const [SliderValueProvider, useSliderValue] = createContext<SliderValueContext>({
+  strict: true,
+  name: 'SliderValueContext',
+});
+
+type SliderThumbContext = {
+  dragging: boolean;
+  onThumbMouseDown: (index?: number) => void;
+};
+
+const [SliderThumbProvider, useSliderThumb] = createContext<SliderThumbContext>({
+  strict: true,
+  name: 'SliderThumbContext',
+});
---
+++ packages/ui/src/components/slider/SliderThumb.tsx
@@ -0,0 +1,233 @@
+import { __DEV__, ariaAttr, clamp, runIfFn } from '@resolid-remix/utils';
+import { forwardRef, useCallback, type KeyboardEvent, type ReactNode } from 'react';
+import { useFocus, useHover, useMergeRefs } from '../../hooks';
+import { clsx } from '../../utils/classed';
+import type { Color } from '../../utils/types';
+import type { Props } from '../slot/Slot';
+import { Tooltip } from '../tooltip/Tooltip';
+import { SliderColorStyles } from './Slider.style';
+import { useSlider, useSliderThumb, useSliderValue, type SliderValue } from './SliderContext';
+
+type SliderThumbButtonProps = {
+  value: number;
+  max: number;
+  min: number;
+  color: Color;
+  vertical: boolean;
+  reverse: boolean;
+  index?: number;
+} & SliderThumbChild;
+
+type SliderThumbChild = {
+  children?: ReactNode | ((index: number | undefined) => ReactNode);
+};
+
+const SliderThumbButton = forwardRef<
+  HTMLButtonElement,
+  Props<'button', SliderThumbButtonProps, 'role' | 'type' | 'tabIndex'>
+>((props, ref) => {
+  const { className, disabled, color, value, max, min, vertical, reverse, children, index, ...rest } = props;
+
+  const [focusRef, focus] = useFocus();
+  const [hoverRef, hover] = useHover();
+
+  const refs = useMergeRefs(ref, hoverRef, focusRef);
+  const { dragging, onThumbMouseDown } = useSliderThumb();
+
+  const colorStyle = SliderColorStyles[color];
+
+  return (
+    <Tooltip.Root opened={focus || hover || dragging} placement={vertical ? 'right' : 'top'}>
+      <Tooltip.Trigger asChild>
+        <button
+          disabled={disabled}
+          tabIndex={0}
+          aria-disabled={ariaAttr(disabled)}
+          aria-valuemax={max}
+          aria-valuemin={min}
+          aria-valuenow={value}
+          aria-orientation={vertical ? 'vertical' : 'horizontal'}
+          role={'slider'}
+          type={'button'}
+          ref={refs}
+          onMouseDown={(event) => {
+            event.stopPropagation();
+            onThumbMouseDown(index);
+          }}
+          onTouchStart={(event) => {
+            event.stopPropagation();
+            onThumbMouseDown(index);
+          }}
+          onClick={(event) => event.stopPropagation()}
+          className={clsx(
+            'absolute z-[2] touch-none select-none rounded-full bg-white outline-none transition-transform',
+            `focus-visible:z-10 focus-visible:ring disabled:border-bg-muted`,
+            colorStyle.thumb,
+            !disabled && 'hover:scale-110',
+            vertical ? 'left-1/2 -translate-x-1/2' : 'top-1/2 -translate-y-1/2',
+            vertical
+              ? reverse
+                ? 'translate-y-1/2'
+                : '-translate-y-1/2'
+              : reverse
+                ? 'translate-x-1/2'
+                : '-translate-x-1/2',
+            !children && 'aspect-square border-3',
+            !children && (vertical ? 'w-1/2' : 'h-1/2 w-fit'),
+            className,
+          )}
+          {...rest}
+        >
+          {runIfFn(children, index)}
+        </button>
+      </Tooltip.Trigger>
+      <Tooltip.Content>
+        <Tooltip.Arrow />
+        {value}
+      </Tooltip.Content>
+    </Tooltip.Root>
+  );
+});
+
+if (__DEV__) {
+  SliderThumbButton.displayName = 'SliderThumbButton';
+}
+
+export const SliderThumb = forwardRef<
+  HTMLButtonElement,
+  Props<'button', SliderThumbChild, 'value' | 'color' | 'type' | 'role' | 'tabIndex'>
+>((props, ref) => {
+  const { className, ...rest } = props;
+
+  const { max, min, color, step, reverse, disabled, vertical } = useSlider();
+  const { value, onChange, onChangeEnd } = useSliderValue();
+
+  const handelKeyDown = useCallback(
+    (event: KeyboardEvent, index?: number) => {
+      if (event.code == 'ArrowDown' || event.key == 'ArrowRight') {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const next: SliderValue = Array.isArray(value)
+          ? index == 0
+            ? [clamp(reverse ? value[0] - step : value[0] + step, [min, value[1]]), value[1]]
+            : [value[0], clamp(reverse ? value[1] - step : value[1] + step, [value[0], max])]
+          : clamp(reverse ? value - step : value + step, [min, max]);
+
+        onChange(next);
+        onChangeEnd(next);
+
+        return;
+      }
+
+      if (event.code == 'ArrowUp' || event.key == 'ArrowLeft') {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const next: SliderValue = Array.isArray(value)
+          ? index == 0
+            ? [clamp(reverse ? value[0] + step : value[0] - step, [min, value[1]]), value[1]]
+            : [value[0], clamp(reverse ? value[1] + step : value[1] - step, [value[0], max])]
+          : clamp(reverse ? value + step : value - step, [min, max]);
+
+        onChange(next);
+        onChangeEnd(next);
+
+        return;
+      }
+
+      if (event.code == 'Home') {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const next: SliderValue = Array.isArray(value) ? (index == 0 ? [min, value[1]] : [value[0], value[0]]) : min;
+
+        onChange(next);
+        onChangeEnd(next);
+      }
+
+      if (event.code == 'End') {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const next: SliderValue = Array.isArray(value) ? (index == 0 ? [value[1], value[1]] : [value[0], max]) : max;
+
+        onChange(next);
+        onChangeEnd(next);
+      }
+    },
+    [value, reverse, step, min, max, onChange, onChangeEnd],
+  );
+
+  const styleAttr = vertical ? (reverse ? 'bottom' : 'top') : reverse ? 'right' : 'left';
+
+  if (Array.isArray(value)) {
+    return (
+      <>
+        <SliderThumbButton
+          ref={ref}
+          className={className}
+          disabled={disabled}
+          value={value[0]}
+          max={value[1]}
+          index={0}
+          min={min}
+          vertical={vertical}
+          reverse={reverse}
+          style={{
+            [styleAttr]: (value[0] / max) * 100 + '%',
+          }}
+          onKeyDown={(event) => {
+            handelKeyDown(event, 0);
+          }}
+          color={color}
+          {...rest}
+        />
+        <SliderThumbButton
+          ref={ref}
+          className={className}
+          disabled={disabled}
+          value={value[1]}
+          max={max}
+          min={value[0]}
+          index={1}
+          vertical={vertical}
+          reverse={reverse}
+          style={{
+            [styleAttr]: (value[1] / max) * 100 + '%',
+          }}
+          onKeyDown={(event) => {
+            handelKeyDown(event, 1);
+          }}
+          color={color}
+          {...rest}
+        />
+      </>
+    );
+  } else {
+    return (
+      <SliderThumbButton
+        ref={ref}
+        className={className}
+        disabled={disabled}
+        value={value}
+        max={max}
+        min={min}
+        vertical={vertical}
+        reverse={reverse}
+        style={{
+          [styleAttr]: (value / max) * 100 + '%',
+        }}
+        onKeyDown={(event) => {
+          handelKeyDown(event);
+        }}
+        color={color}
+        {...rest}
+      />
+    );
+  }
+});
+
+if (__DEV__) {
+  SliderThumb.displayName = 'SliderThumb';
+}
