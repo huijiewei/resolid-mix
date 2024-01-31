54f998ca04e2d75d5413907b374e0bfb2681b6d6
Huijie Wei <huijiewei@outlook.com>
2024-01-31 21:28:28 +0800

    refactor(ui): optimize Avatar fallback font size

--- packages/ui/src/components/avatar/Avatar.style.ts
+++ packages/ui/src/components/avatar/Avatar.style.ts
@@ -3,8 +3,8 @@ import { isDarkColor, randomColor } from '../../utils/color';
 import type { Size, Styled } from '../../utils/types';
 
 const avatarSizeStyles = {
-  xs: 'h-4 w-4 text-[calc(theme(height.4)/3)]',
-  sm: 'h-7 w-7 text-[calc(theme(height.7)/3)]',
+  xs: 'h-4 w-4 text-[calc(theme(height.4)/2)]',
+  sm: 'h-7 w-7 text-[calc(theme(height.7)/2)]',
   md: 'h-10 w-10 text-[calc(theme(height.10)/3)]',
   lg: 'h-14 w-14 text-[calc(theme(height.14)/3)]',
   xl: 'h-20 w-20 text-[calc(theme(height.20)/3)]',
@@ -14,7 +14,9 @@ export const toSized = (size: number | Size): Styled => {
   if (isNumber(size)) {
     return {
       value: `${size}px`,
-      style: 'w-[--size-var] h-[--size-var] text-[length:calc(var(--size-var)/3)]',
+      style:
+        'w-[--size-var] h-[--size-var] ' +
+        (size > 50 ? 'text-[length:calc(var(--size-var)/3)]' : 'text-[length:calc(var(--size-var)/2)]'),
     };
   }
 
