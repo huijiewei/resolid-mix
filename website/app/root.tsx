9d80ef3ea06506a9abb3957a701cf4473bc85c9d
Huijie Wei <huijiewei@outlook.com>
2024-02-08 17:10:08 +0800

    chore: update tailwindcss config

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -101,7 +101,7 @@ export default function App() {
     <html lang="zh-CN" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0969da" />
         <Meta />
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -3,14 +3,14 @@ import type { Config } from 'tailwindcss';
 import resolidMix from '../packages/tailwind/src';
 
 // noinspection JSUnusedGlobalSymbols
-export default {
+export default <Partial<Config>>{
   presets: [resolidMix.preset()],
   content: [
     './app/**/*.{js,ts,tsx}',
-    './node_modules/@resolid/mix-ui/dist/*.{js,cjs,mjs}',
-    './node_modules/@resolid/mix-ui/src/**/*.{ts,tsx,mdx}',
+    './node_modules/@resolid/mix-ui/dist/*.{js}',
+    './node_modules/@resolid/mix-ui/src/**/*.{ts,tsx}',
   ],
   safelist: ['scroll-mt-20', 'text-green-500', 'ml-2', 'group-hover:opacity-100'],
   theme: {},
   plugins: [typography],
-} satisfies Config;
+};
